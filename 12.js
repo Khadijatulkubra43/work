08ea7b2f8fb053014bd6b7c2d66c5d9da9f76163
Khadija tul kubra <89134926+Khadijatulkubra43@users.noreply.github.com>
2023-01-18 14:05:06 +0000

    Add files via upload

---
+++ 12.js
@@ -0,0 +1,14 @@
+document.getElementById("myBtn").click();
+function myFunction(id) {
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function likeFunction(x) {
+  x.style.fontWeight = "bold";
+  x.innerHTML = "✓ Liked";
+}
